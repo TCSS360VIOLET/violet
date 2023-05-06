59aa3d98f3258a83acb11909cf7e2fe39d613d75
aho999 <122649664+aho999@users.noreply.github.com>
2023-05-06 11:11:01 -0700

    test

--- model/Team.java
+++ model/Team.java
@@ -6,31 +6,41 @@ import java.util.Map;
 /* NOTE TO OTHER DEVELOPERS:
  * Be sure to add your emails into the developers HashMap shown below in the constructor!
  * 
- * To get each team member's info from this class,
- * just call a for-each style loop like so on the HashMap...
+ * You have two ways of getting team info from this class:
+ * 1) Just call a for-each style loop like so on the HashMap...
  * 
- * for(Map.Entry<Integer, String> entry : Team.getDevelopers().entrySet()) {
+ * for(Map.Entry<Integer, String> entry : Team.getDevelopersMap().entrySet()) {
  * 		String member = entry.getValue(); //This will give the string "FIRST NAME LAST NAME (EMAIL@uw.edu)" for that member.
  * 		//Do stuff with this member's info here
  * }
+ * 
+ * 2) There is also an option for just a String[] array of each memeber using getDevelopersArray().
  */
 
 /**
  * This class will hold information on the team members/developers on Team violet.
+ * It allows us to keep our personal and contact info in once central place, so
+ * we wont need to change it in multiple files.
  * 
  * @author Nickolas Zahos (nzahos@uw.edu)
  * @author An Ho (aho1999@uw.edu)
+<<<<<<< HEAD
  * 
+=======
+ * @author Edward Chung (edward92@uw.edu)
+ * @author Lixin Wang
+ * @author Parker Johnson (ptj7@uw.edu)
+>>>>>>> 50c28be434f998b91a1f90f6e3310b39cace73f6
  */
 public class Team {
 	/** This map will hold the developer(s) info as "FIRST NAME LAST NAME, EMAIL" */
-	private Map<Integer, String> developers;
+	private final Map<Integer, String> developers;
 	
 	/** Default Constructor - Takes no parameters, but has all team info stored. */
 	public Team() {
 		developers = new HashMap<Integer, String>();
 		
-		//ADD YOUR PERSONAL INFO HERE!
+		// ADD YOUR PERSONAL INFO HERE! Ex: First Last (email)
 		developers.put(1, "Nickolas Zahos (nzahos@uw.edu)");
 		developers.put(2, "An Ho (aho1999@uw.edu)");
 		developers.put(3, "Edward Chung (edward92@uw.edu)");
@@ -39,12 +49,27 @@ public class Team {
 	}
 	
 	/**
-	 * This method returns a defensive copy of the map of developer(s) info.
+	 * This get method returns a defensive copy of the map of developer(s) info.
 	 * 
 	 * @return	a defensive copy of developers HashMap.
 	 */
-	public Map<Integer, String> getDevelopers() {
-		//Defensive Copy
+	public Map<Integer, String> getDevelopersMap() {
+		// Return Defensive Copy
 		return new HashMap<>(developers);
+	}
+
+	/**
+	 * This get methods returns a String array with each of the developer's info as a seperate element.
+	 */
+	public String[] getDevelopersArray() {
+		String[] devsArray = new String[developers.size()];
+		int i = 0;
+		
+		for(Map.Entry<Integer, String> entry : developers.entrySet()) {
+			devsArray[i] = entry.getValue();
+			i++;
+		}
+
+		return devsArray;
 	}
 }
