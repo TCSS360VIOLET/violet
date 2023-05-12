86bec2f198dbdcff6b2f43442b495c39b5f9700b
Nickolas Zahos <nzahos@yahoo.com>
2023-05-11 18:20:16 -0700

    Nick: Resizing fix

--- model/About.java
+++ model/About.java
@@ -8,11 +8,13 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -26,99 +28,148 @@ import java.awt.event.ActionListener;
  * @author Edward Chung
  */
 public class About {
-    private static final double VERSION_NUMBER = 1.1;
-    private final JFrame myFrame;
+    /** The main JFrame of this About window. */
+	private JFrame myFrame;
+
+    /** This string array will hold all the developer's info. */
     private final String[] developers;
-
-    public About() {
-        // Get the team info for later
+	
+	/** Default Contructor - Takes no parameters, sets up the UI for this frame. */
+	public About() {
+		myFrame = new JFrame("About");
         developers = new Team().getDevelopersArray();
-
-        // Setup the About JFrame
-        myFrame = new JFrame("About");
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Set the Window icon
-        myFrame.setIconImage(Toolkit.getDefaultToolkit().getImage("images/icon.png"));
-
-        
-		// Set uniform window size across different screen resolutions (Bad for Ultra-widescreen monitors, could stretch)
+		
+		// Set the JFrame icon to my custom icon.
+		URL iconURL = getClass().getResource("/images/icon.png");
+        ImageIcon myIcon = new ImageIcon(iconURL);
+		myFrame.setIconImage(myIcon.getImage());
+		
+		// Closing the window exits this program/frame.
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// For uniform window size across different screen resolutions (Bad for Ultra-widescreen monitors, could stretch)
 		// Get screen dimensions
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int screenHeight = screenSize.height;
         int screenWidth = screenSize.width;
         
-        // Sets this JFrame size to 1/3 the width of the user's screen, and half it's height.
+        // Sets this JFrame size to 1/4 the width of the user's screen, and half it's height.
         myFrame.setSize(screenWidth / 4, screenHeight / 2);
         
-        // Position the frame in the center of the screen by setting location to null.
-        myFrame.setLocationRelativeTo(null);
-
-        // Text basic description about our software. 
-        String descriptionText = "Our software is a powerful project management tool designed to help you seamlessly organize, track, and prioritize multiple projects. With intuitive features for setting deadlines, managing budgets, monitoring purchased items, storing designs and files, and adding notes for each project, it's the ultimate solution for staying on top of your work and achieving your goals. Plus, our convenient import/export functionality allows you to effortlessly sync your project data across multiple devices, ensuring you have access to the information you need, whenever and wherever you need it.<br>" +
-                                 "<br>Brought to you by Team Violet!<br>" + 
-                                 "<br>- - - Developers - - -<br>"; 
-
-        // Add each Developer's name and contact info.
-        for(int i = 0; i < developers.length; i++) {
-            descriptionText += developers[i] + "<br>";
-        }
-
-        // Add version number of the current build of the program.
-        descriptionText += "<br>Version " + VERSION_NUMBER;
-
-        JLabel descriptionLabel = new JLabel("<html><div style='text-align: center;'>" + descriptionText + "</div></html>", SwingConstants.CENTER);
-
-        // Add padding to the border of the JLabel (makes it look nicer).
-        descriptionLabel.setBorder(new EmptyBorder(0, 20, 10, 20));
-
-        myFrame.getContentPane().add(descriptionLabel, BorderLayout.CENTER);
-
-        // Logo image 
-        URL logoURL = getClass().getResource("/images/logo.png");
-        ImageIcon image = new ImageIcon(logoURL);
-        JLabel logoImage = new JLabel(image);
-        myFrame.getContentPane().add(logoImage, BorderLayout.NORTH);
-
-        // Add 'Ok' button to the bottom of the JFrame
+        // Create a JPanel with BorderLayout
+        JPanel myPanel = new JPanel(new BorderLayout());
+        
+        // Set up the logo.
+        setupLogo(myPanel);
+        
+        // Set up the text field.
+        setupTextField(myPanel);
+        
+        // Set up the 'Ok' button.
+        setupButton(myPanel);
+	}
+	
+	/**
+	 * This private method will only be called by the constructor.
+	 * It just places an 'Ok' button below the text field.
+	 * @param panel The panel that the component will be attached to.
+	 */
+	private void setupButton(JPanel panel) {
+        // Create the 'Ok' button
         JButton okButton = new JButton("Ok");
-
-        // Action Listener that makes the About window close when you click the 'Ok' button
         okButton.addActionListener(new ActionListener() {
-            @Override
             public void actionPerformed(ActionEvent e) {
-                exit(); // Close the About window
+                myFrame.dispose();
             }
         });
 
-        JPanel buttonPanel = new JPanel();
+        // Create a new JPanel with FlowLayout to hold the button
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
-        // Add lower padding to the 'Ok' button, makes it look nicer.
-        buttonPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
-
+        // Add the 'Ok' button to the button panel
         buttonPanel.add(okButton);
-        myFrame.add(buttonPanel, BorderLayout.SOUTH);
 
-        // Ignores the screen size we set earlier to pack the window to the neceasary size to fit all its contents (Bad format for long descriptions like ours, causes stretched window).
-        //myFrame.pack();
-   }
+        // Add the button panel to the main panel using BorderLayout.SOUTH
+        panel.add(buttonPanel, BorderLayout.SOUTH);
+	}
+	
+	/**
+	 * This private method will only be called by the constructor.
+	 * It just sets up the large logo that will appear above the text field.
+	 * @param panel The panel that the component will be attached to.
+	 */
+	private void setupLogo(JPanel panel) {
+        // Load the logo image and create an ImageIcon
+        URL logoURL = getClass().getResource("/images/logo.png");
+        ImageIcon logoIcon = new ImageIcon(logoURL);
+        
+        // Resize the ImageIcon
+        Image image = logoIcon.getImage();
+        double aspectRatio = (double) image.getWidth(null)/(double) image.getHeight(null);
+        
+        // Here, we're going to scale the image to 3/4 of the JFrame's height
+        int scaledHeight = (int) (myFrame.getHeight() * 0.4);
+        int scaledWidth = (int) (scaledHeight * aspectRatio);
 
-   /**
-    * This basic method will show/hide the main JFrame (About) window
-    * depending on which boolean is passed as a parameter.
-    * NOTE: Hiding the JFrame does NOT close it (will still be taking memory in the background)!
-    *
-    * @param b  true = show, false = hide.
-    */
-   public void show(boolean b) {
-        myFrame.setVisible(b); 
-   }
+        Image scaledImage = image.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH);
+        logoIcon = new ImageIcon(scaledImage);
+        
+        // Create a JLabel with the ImageIcon and add it to the JPanel using BorderLayout.NORTH
+        JLabel logoLabel = new JLabel(logoIcon);
+        panel.add(logoLabel, BorderLayout.NORTH);
+	}
 
-   /**
-    * This basic method will close/exit the JFrame for the about window.
-    * Completely exits it out, restoring all memory that was being used.
-    */
-   public void exit() {
-        myFrame.dispose();
-   }
+	/**
+	 * This private method will only be called by the constructor.
+	 * It just sets up the text field of the about JFrame that will
+	 * describe the program.
+	 * @param panel The panel that the component will be attached to.
+	 */
+	private void setupTextField(JPanel panel) {
+        String descriptionText = "Our software is a powerful project management tool designed to help you seamlessly organize, track, and prioritize multiple projects. With intuitive features for setting deadlines, managing budgets, monitoring purchased items, storing designs and files, and adding notes for each project, it's the ultimate solution for staying on top of your work and achieving your goals. Plus, our convenient import/export functionality allows you to effortlessly sync your project data across multiple devices, ensuring you have access to the information you need, whenever and wherever you need it." + 
+        "\nBrought to you by Team Violet!\n" + 
+        "\n- - - Developers - - -\n"; 
+        
+        // Add each Developer's name and contact info.
+        for(int i = 0; i < developers.length; i++) {
+            descriptionText += developers[i] + "\n";
+        }
+
+        JTextArea aboutTextArea = new JTextArea(descriptionText);
+
+        aboutTextArea.setEditable(false);
+        aboutTextArea.setLineWrap(true);
+        aboutTextArea.setWrapStyleWord(true);
+        
+        // Set padding around the text using an empty border
+        int padding = 25;
+        aboutTextArea.setBorder(BorderFactory.createEmptyBorder(padding, padding, padding, padding));
+
+        // Wrap the JTextArea in a JScrollPane
+        JScrollPane scrollPane = new JScrollPane(aboutTextArea);
+        
+        // Set padding around the JScrollPane using an empty border
+        int scrollPanePadding = 20;
+        scrollPane.setBorder(BorderFactory.createEmptyBorder(0, scrollPanePadding, scrollPanePadding, scrollPanePadding));
+
+
+        // Add the JScrollPane to the JPanel using BorderLayout.CENTER
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        // Add the JPanel to the JFrame
+        myFrame.add(panel);
+	}
+	
+	/**
+	 * This method displays this class's JFrame.
+     * 
+     * @param   b   True for show, false for hide frame.
+	 */
+	public void show(boolean b) {
+        // Center the JFrame to the UI Object (null means just center of screen)
+        myFrame.setLocationRelativeTo(null);
+
+        // Show the JFrame
+        myFrame.setVisible(b);
+	}
 }
