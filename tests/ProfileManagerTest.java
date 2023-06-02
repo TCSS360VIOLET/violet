a45fdcd6a1d2257a65d075cbd5cc0c813030d732
parker7johnson <parker7johnson@hotmail.com>
2023-06-01 18:52:39 -0700

    Adding junit bodies

---
+++ tests/ProfileManagerTest.java
@@ -0,0 +1,102 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfileManagerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void loadUserFile() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addProfile() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addProject() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addFilePath() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addProjectNotes() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProjects() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProjectItems() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProjectFilePaths() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProjectStartDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProjectEndDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProjectBudget() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getItemDescription() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getItemCostPerUnit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getItemQuantity() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProjectNotes() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteProject() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteFilePath() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setProjectName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setProjectStartDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setProjectEndDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setProjectBudget() {
+    }
+}
